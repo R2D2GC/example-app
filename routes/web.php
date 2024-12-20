ab8937932dcec8e00b3e1cd6404596ef1ca7287d
R2D2GC <ingcervantesarturo@gmail.com>
2024-12-19 22:42:05 -0600

    vv1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hola mundo' ;
+});
+
+Route::get('/posts', function() { //rutas definidas
+    return 'como funcionan las rutas';
+});
+
+Route::get('/posts/create', function(){
+    return "Formulario de creacion";
+});
+
+Route::get('/posts/{post}', function($post){
+    return "rutas variables {$post}";
 });
